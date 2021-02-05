37a19362e106fcd8dd3235a9573cfea7085fac00
azzy9 <19675382+azzy9@users.noreply.github.com>
2021-02-05 17:44:30 +0000

    telnet_control.py - PY3

--- lib/telnet_control.py
+++ lib/telnet_control.py
@@ -9,21 +9,21 @@ class telnet_control:
         self.telnet_conn = telnetlib.Telnet(self.host,self.port)
 
     def login(self, user, password=""):
-        self.telnet_conn.read_until("login: ")
-        self.telnet_conn.write(user + "\r\n")
-        self.telnet_conn.read_until("Password: ")
-        self.telnet_conn.write(password + "\r\n")
+        self.telnet_conn.read_until(b"login: ")
+        self.telnet_conn.write(user.encode('ascii') + b"\r\n")
+        self.telnet_conn.read_until(b"Password: ")
+        self.telnet_conn.write(password.encode('ascii') + b"\r\n")
 
     def server_start(self):
-        self.telnet_conn.write("SmartHub_DS /mnt/cdrom\r\n")
+        self.telnet_conn.write(b"SmartHub_DS /mnt/cdrom\r\n")
 
     def server_stop(self):
-        self.telnet_conn.write("killall SmartHub_DS \r\n")
+        self.telnet_conn.write(b"killall SmartHub_DS \r\n")
 
     def exit(self):
-        self.telnet_conn.write("vt100\r\n")
+        self.telnet_conn.write(b"vt100\r\n")
 
-        self.telnet_conn.write("ls\r\n")
-        self.telnet_conn.write("exit\r\n")
-        xbmc.log(self.telnet_conn.read_all())
+        self.telnet_conn.write(b"ls\r\n")
+        self.telnet_conn.write(b"exit\r\n")
+        xbmc.log(str(self.telnet_conn.read_all()))
         self.telnet_conn.close()
