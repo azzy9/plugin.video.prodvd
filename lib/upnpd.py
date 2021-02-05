e1d2f57766b2d18eb99aba70011eeb20bf60b10d
azzy9 <19675382+azzy9@users.noreply.github.com>
2021-02-05 17:45:10 +0000

    upnpd.py - PY3

--- lib/upnpd.py
+++ lib/upnpd.py
@@ -1,34 +1,32 @@
-#   Copyright 2014 Dan Krause
-#   Has been modified
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-import urllib2, urllib, re
-import socket, httplib, StringIO
+import re
+import socket
 import xbmc
-from urlparse import urlparse
+import requests
+import six
+import io
+
+if six.PY3:
+    import http.client
+else:
+    import httplib
+
+from six.moves import urllib
+from six.moves import urllib_parse
 
 class SSDPResponse(object):
 
-    class _FakeSocket(StringIO.StringIO):
+    class _FakeSocket(io.BytesIO):
         def makefile(self, *args, **kw):
             return self
 
     def __init__(self, response):
-        r = httplib.HTTPResponse(self._FakeSocket(response))
+        if six.PY3:
+            r = http.client.HTTPResponse(self._FakeSocket(response))
+        else:
+            r = httplib.HTTPResponse(self._FakeSocket(response))
         r.begin()
         self.location = r.getheader("location")
-        self.domain = urlparse( r.getheader("location") ).hostname
+        self.domain = urllib_parse.urlparse( r.getheader("location") ).hostname
         self.usn = r.getheader("usn")
         self.st = r.getheader("st")
         self.cache = r.getheader("cache-control").split("=")[1]
@@ -50,7 +48,7 @@ def discover(service, timeout=5, retries=1, mx=3):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-        sock.sendto(message.format(*group, st=service, mx=mx), group)
+        sock.sendto(message.format(*group, st=service, mx=mx).encode('utf-8'), group)
         while True:
             try:
                 response = SSDPResponse(sock.recv(1024))
@@ -61,13 +59,12 @@ def discover(service, timeout=5, retries=1, mx=3):
 
 #get the xml file
 def XmlGet(url):
-    response = urllib2.urlopen(url)
-    xml = response.read()
-    return xml
+    response = urllib.request.urlopen(url)
+    return str(response.read())
 
 #parse xml to get the UUID
 def UUIDGet(xml):
-    match = re.search('<UDN>uuid:([a-z0-9-]+)</UDN>', xml)
+    match = re.search(r'<UDN>uuid:([a-z0-9-]+)</UDN>', xml)
     if match:
         return match.group(1)
 
@@ -100,7 +97,7 @@ def SOAPCall( host, path, urn, action, soap_body = "" ):
 
     xbmc.log(soap_body)
     xbmc.log(host + path)
-    request = urllib2.Request(host + path, soap_body, headers)
-    response = urllib2.urlopen(request)
+    request = urllib.request.Request(host + path, soap_body.encode("utf-8"), headers)
+    response = urllib.request.urlopen(request)
 
     return response.read()
