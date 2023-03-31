1a620326b95495d74a4edc74e084e5dca6b2fa99
azzy9 <19675382+azzy9@users.noreply.github.com>
2023-03-31 10:23:37 +0100

    default.py - make pylint compliant

--- default.py
+++ default.py
@@ -1,148 +1,177 @@
-import sys, os, re
-import xbmc, xbmcgui
-import binascii, tempfile
+""" Main code for running plugin"""
+import sys
+import os
+import re
+
+import binascii
+import tempfile
+
+import xbmc
+import xbmcgui
+
 import six
 
 from six.moves import urllib
 from lib import telnet_control, upnpd, ProDVD
 
-addon_id='plugin.video.prodvd'
-device_model = "[ODD] SmartHub 208BW"
-ProDVDServer = False
+ADDON_ID='plugin.video.prodvd'
+DEVICE_MODEL = "[ODD] SmartHub 208BW"
+PRODVD_SERVER = False
 
-ProDVDServerTest = False
-#ProDVDServerTest = True
-ProDVDSectorBuffer = 1000
+PRODVD_SERVER_TEST = False
+#PRODVD_SERVER_TEST = True
+PRODVD_SECTOR_BUFFER = 1000
 
 #params being set
 params = dict( urllib.parse.parse_qsl(sys.argv[2].replace('?','')) )
 action = params.get('action')
 fileid = params.get('fileid')
 
-#method to start a ProDVD server
-def ProDVD_start():
+def prodvd_start():
 
-    #initiate ProDVDServer
-    ProDVDServer = False
+    """ method to start a ProDVD server """
+
+    #initiate PRODVD_SERVER
+    PRODVD_SERVER = False
 
     #use SDPP to find a server to start
     xbmc.log("Starting UPNP search")
-    UPNP_found = upnpd.discover("urn:schemas-upnp-org:device:MediaServer")
+    upnp_found = upnpd.discover("urn:schemas-upnp-org:device:MediaServer")
 
     xbmc.log("Looping Results")
-    if UPNP_found:
-        for upnp in UPNP_found:
+    if upnp_found:
+        for upnp in upnp_found:
             #not sure if I like how it is doing this, but it work
-            if device_model in upnpd.XmlGet(upnp.location):
+            if DEVICE_MODEL in upnpd.XmlGet(upnp.location):
                 #we found what we are looking for
-                ProDVDServer = upnp.domain
+                PRODVD_SERVER = upnp.domain
                 break
- 
-    if ProDVDServer != False:
+
+    if PRODVD_SERVER is not False:
         #start the server
-        tnc = telnet_control.telnet_control(ProDVDServer,23)
+        tnc = telnet_control.telnet_control(PRODVD_SERVER, 23)
         tnc.login("root")
         #start the UPNP ProDVD server
         tnc.server_start()
         tnc.exit()
 
-#method to find a ProDVD server
-def ProDVD_find( retries=1 ):
-    #find if an instance of the ProDVD server has been started
-    UPNP_found = upnpd.discover("urn:schemas-upnp-org:service:ProDVDContentDirectory:1", 5, retries)
+def prodvd_find( retries=1 ):
 
-    if UPNP_found:
+    """ method to find a ProDVD server """
+
+    #find if an instance of the ProDVD server has been started
+    upnp_found = upnpd.discover("urn:schemas-upnp-org:service:ProDVDContentDirectory:1", 5, retries)
+
+    if upnp_found:
         xbmc.log("ProDVD server found")
-        for upnp in UPNP_found: 
+        for upnp in upnp_found:
             xbmc.log(upnp.location)
             return upnp.location
     else :
         return False
 
-#method to play from the ProDVD server
-def ProDVD_play( url ):
+def prodvd_play( url ):
 
-    UUID = upnpd.UUIDGet(upnpd.XmlGet(url))
-    if UUID != False:
+    """ method to play from the ProDVD server """
 
-        dvd_title = ProDVD.getMediaName(UUID, url)
+    uuid = upnpd.UUIDGet(upnpd.XmlGet(url))
+    if uuid is not False:
 
-        soap = ProDVD.browse(UUID, url)
-        match = re.findall(r'item(?:\s+)id="(?P<fileid>[^"]+)"(?:(?:.|\n|\r)+?)(?P<url>' + urllib.parse.urlparse( url ).scheme + "://" + urllib.parse.urlparse( url ).netloc + '[^"]+.vob)', soap)
+        dvd_title = ProDVD.getMediaName(uuid, url)
+
+        soap = ProDVD.browse(uuid, url)
+        match = re.findall(r'item(?:\s+)id="(?P<fileid>[^"]+)"(?:(?:.|\n|\r)+?)(?P<url>'
+                           + urllib.parse.urlparse( url ).scheme
+                           + "://" + urllib.parse.urlparse( url ).netloc + '[^"]+.vob)', soap)
         if match:
             xbmc.log(dvd_title)
             playlist_create(dvd_title, match)
             xbmc.log("ProDVD_play RAN!!!!!!")
 
-    return UUID
+    return uuid
 
-def playlist_create( dvd_title, list ):
+def playlist_create( dvd_title, play_list ):
+
+    """ Method to create playlist """
+
     playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
     playlist.clear()
-    for fileid, url in list:
+    for file_id, url in play_list:
         listitem = xbmcgui.ListItem(dvd_title)
         listitem.setInfo( type="Video", infoLabels={ "Title": dvd_title } )
-        if ProDVDServerTest == True :
-            url = "plugin://" + addon_id + "/?action=play_file&fileid=" + fileid
+        if PRODVD_SERVER_TEST is True :
+            url = "plugin://" + ADDON_ID + "/?action=play_file&fileid=" + file_id
             xbmc.executebuiltin('RunPlugin(%s)' % url)
             return
 
         playlist.add( urllib.parse.unquote_plus( url ), listitem)
 
-    xbmcPlayer = xbmc.Player()
-    xbmcPlayer.play(playlist)
+    xbmc_player = xbmc.Player()
+    xbmc_player.play(playlist)
 
-def play_file( fileid ):
+def play_file( file_id ):
+
+    """ Play Files (test) """
+
     xbmc.log("Play File Test Start")
-    ProDVDServer = ProDVD_find()
 
-    if ProDVDServer == False:
-        ProDVD_start()
+    PRODVD_SERVER = prodvd_find()
+
+    if PRODVD_SERVER is False:
+        prodvd_start()
         #let's try this again
         #increase retries as server just started
         #may need some time to warm up
-        ProDVDServer = ProDVD_find(3)
-    if ProDVDServer == False:
+        PRODVD_SERVER = prodvd_find(3)
+
+    if PRODVD_SERVER is False:
         #oh no!
         xbmc.log("ProDVD server can not be found / started")
     else:
-        UUID = upnpd.UUIDGet(upnpd.XmlGet(ProDVDServer))
-        if UUID != False:
+        uuid = upnpd.UUIDGet(upnpd.XmlGet(PRODVD_SERVER))
+        if uuid is not False:
 
-            fd, path = tempfile.mkstemp()
+            fdir, path = tempfile.mkstemp()
 
-            with os.fdopen(fd, 'wb') as tmp:
+            with os.fdopen(fdir, 'wb') as tmp:
 
-                for x in range(0, 20):
-                    sectors = ProDVD.readDataByFileOffset(UUID, ProDVDServer, fileid, str( ProDVDSectorBuffer * x ), str( ( ProDVDSectorBuffer * ( x + 1 ) ) - 1 ))
+                for b_num in range(0, 20):
+                    sectors = ProDVD.readDataByFileOffset(
+                        uuid, PRODVD_SERVER, file_id,
+                        str( PRODVD_SECTOR_BUFFER * b_num ),
+                        str( ( PRODVD_SECTOR_BUFFER * ( b_num + 1 ) ) - 1 )
+                    )
                     binary_string = binascii.unhexlify(sectors)
                     # do stuff with temp file
                     tmp.write(binary_string)
 
                 playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-                listitem = xbmcgui.ListItem(fileid)
-                xbmc.log( str( fd ), xbmc.LOGWARNING )
+                listitem = xbmcgui.ListItem(file_id)
+                xbmc.log( str( fdir ), xbmc.LOGWARNING )
                 xbmc.log( str( path ), xbmc.LOGWARNING )
                 playlist.add( str( path ), listitem)
-                xbmcPlayer = xbmc.Player()
-                xbmcPlayer.play(playlist)
+                xbmc_player = xbmc.Player()
+                xbmc_player.play(playlist)
                 #xbmc.log(sectors, xbmc.LOGWARNING)
 
 def main():
-    ProDVDServer = ProDVD_find()
 
-    if ProDVDServer == False:
-        ProDVD_start()
+    """ Main method """
+
+    PRODVD_SERVER = prodvd_find()
+
+    if PRODVD_SERVER is False:
+        prodvd_start()
         #let's try this again
         #increase retries as server just started
         #may need some time to warm up
-        ProDVDServer = ProDVD_find(3)
-    if ProDVDServer == False:
+        PRODVD_SERVER = prodvd_find(3)
+    if PRODVD_SERVER is False:
         #oh no!
         xbmc.log("ProDVD server can not be found / started")
     else:
         #all is fine now, phew
-        ProDVD_play(ProDVDServer)
+        prodvd_play(PRODVD_SERVER)
 
 if action == 'play_file':
     play_file( urllib.parse.unquote_plus( fileid ) )
